6ba1ef9f91647f6d9744f6e88b006afc350dbebe
Dimitar Petkov <dimitar.petkov@axsmarine.com>
2022-12-06 09:32:40 +0200

    added https

--- app/api.py
+++ app/api.py
@@ -17,7 +17,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "localhost:3000",
-    "fastapi-demo-frontend.vercel.app"
+    "https://fastapi-demo-frontend.vercel.app"
 ]
 
 app.add_middleware(
